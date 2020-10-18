8f2b3747d41fe18dc8eba2e19d268bf380864c0e
Yatish Kelkar <yatish1606@gmail.com>
2020-10-18 18:13:18 +0530

    Added Keeper project

---
+++ Keeper/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SpeakerNotesRoundedIcon from '@material-ui/icons/SpeakerNotesRounded';
+
+function Header () {
+    return(
+        <header>
+            <h1><SpeakerNotesRoundedIcon fontSize='medium'/> Keeper</h1>
+        </header>
+    )
+}
+
+export default Header
